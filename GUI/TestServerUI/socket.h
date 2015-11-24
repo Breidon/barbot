4326323ea245c7534ad1ffb4b1e776fa0149b3df
Chris Wegter <c.wegter@student.fontys.nl>
2015-11-24 09:31:24 +0100

    removed GUI, to be remade

--- GUI/TestServerUI/socket.h
+++
@@ -1,9 +0,0 @@
-#ifndef SOCKET_H
-#define SOCKET_H
-
-extern int CreateServerSocket (unsigned short port); /* Create TCP server socket */
-extern int AcceptTCPConnection (int servSock);  /* Accept TCP connection request */
-extern void HandleTCPClient (int clntSocket);   /* TCP client handling function */
-
-
-#endif // SOCKET_H
